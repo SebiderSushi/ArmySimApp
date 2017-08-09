28e703c2a54b98695f31a8bf51a6f36c08d8b377
Sebastian <SebiderSushi@t-online.de>
2017-08-09 05:50:07 +0200

    Rearrangements

--- app/src/main/java/me/sebi/armysim/Row.java
+++ app/src/main/java/me/sebi/armysim/Row.java
@@ -5,55 +5,20 @@ package me.sebi.armysim;
  */
 class Row {
 
-    final int attack;
-    int lives;
-    int deathRound;
-    final int roundsAfterDeath;
-    final int attackSpeed;
-    boolean ATTACK_WEAKEST_ROW = false;
-    boolean DISTANCE_DAMAGE = true;
-    boolean DISTANCE_FIGHTER = false;
+    final int attack, defense, roundsAfterDeath, attackSpeed, reach;
+    int lives, deathRound;
+    final boolean ATTACK_WEAKEST_ROW, DISTANCE_FIGHTER, DISTANCE_DAMAGE;
 
-    /**
-     * The constructor we'll be using in our App
-     */
-    Row(int attack, int lives, int attackSpeed, int roundsAfterDeath, boolean ATTACK_WEAKEST_ROW, boolean DISTANCE_DAMAGE, boolean DISTANCE_FIGHTER) {
+    Row(int attack, int lives, int defense, int attackSpeed, int roundsAfterDeath,
+        boolean ATTACK_WEAKEST_ROW, boolean DISTANCE_DAMAGE, boolean DISTANCE_FIGHTER, int reach) {
         this.attack = attack;
         this.lives = lives;
+        this.defense = defense;
         this.attackSpeed = attackSpeed;
         this.roundsAfterDeath = roundsAfterDeath;
         this.ATTACK_WEAKEST_ROW = ATTACK_WEAKEST_ROW;
         this.DISTANCE_DAMAGE = DISTANCE_DAMAGE;
         this.DISTANCE_FIGHTER = DISTANCE_FIGHTER;
+        this.reach = reach;
     }
-
-    /*
-    Row(int attack, int lives) {
-        this.attack = attack;
-        this.lives = lives;
-        this.attackSpeed = 0;
-    }
-
-    Row(int attack, int lives, String[] effects) {
-        this(attack, lives);
-        if (effects != null)
-            for (String effect : effects)
-                switch (effect) {
-                    case "NO_DISTANCE_DAMAGE":
-                        this.DISTANCE_DAMAGE = false;
-                    case "ATTACK_WEAKEST":
-                        this.ATTACK_WEAKEST_ROW = true;
-                }
-    }
-
-    Row(int attack, int lives, int attackSpeed) {
-        this(attack, lives);
-        this.attackSpeed = attackSpeed;
-    }
-
-    Row(int attack, int lives, int attackSpeed, String[] effects) {
-        this(attack, lives, effects);
-        this.attackSpeed = attackSpeed;
-    }
-    */
 }
